de94e8b86a0fb078a8ae01440e6554e7af011a59
realib1 <ibeevpersie@gmail.com>
2023-04-30 15:14:34 +0000

    Homepage almost done

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,65 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+window.addEventListener('DOMContentLoaded', () => {
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+  // Responsive Navbar
+  const menu = document.querySelector('.nav-list');
+  const menuBelow = document.querySelector('.nav-below');
+  const bars = document.querySelector('.bars');
+  const open = document.querySelector('.open-btn');
+  const close = document.querySelector('.close-btn');
+  
+  bars.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+      menuBelow.classList.toggle('hidden');
+      open.classList.toggle('hidden');
+      close.classList.toggle('hidden');
+  
+      // close menu when clicking on a link
+      const links = document.querySelectorAll('.nav-list a', '.nav-below a');
+      links.forEach((link) => {
+          link.addEventListener('click', () => {
+              menu.classList.add('hidden');
+              menuBelow.classList.add('hidden');
+              open.classList.remove('hidden');
+              close.classList.add('hidden');
+          });
+      });
+  
+  
+  
+  
+      // close menu when clicking outside
+      window.addEventListener('click', (e) => {
+          if (e.target !== menu && e.target !== bars && e.target !== open && e.target !== close) {
+              menu.classList.add('hidden');
+              menuBelow.classList.add('hidden');
+              open.classList.remove('hidden');
+              close.classList.add('hidden');
+          }
+      });
+  });
+  
+  
+  
+  
+  
+  
+  
+  // Scroll to top button
+  // const scrollToTopBtn = document.querySelector('.scroll-to-top-btn');
+  
+  // window.addEventListener('scroll', () => {
+  //     if (window.pageYOffset > 100) {
+  //         scrollToTopBtn.classList.add('show-scroll-btn');
+  //     } else {
+  //         scrollToTopBtn.classList.remove('show-scroll-btn');
+  //     }
+  // });
+  
+  // scrollToTopBtn.addEventListener('click', () => {
+  //     window.scrollTo({
+  //         top: 0,
+  //         behavior: 'smooth'
+      // });
+  // });
+  
+  });
